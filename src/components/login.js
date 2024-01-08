980f5a73fb4fae0833371b01553c9e5936ac2fef
Soham Dudhat <96675704+Soham1105@users.noreply.github.com>
2024-01-08 17:57:16 +0530

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -3,68 +3,69 @@ import '../style/login.css'
 import '../JS/loginApp'
 
 function MaterialContainer() {
-  return (
-    <div>
-      <div class="materialContainer">
+   return (
+      <div>
+         <h1>Welcome to Attendance System</h1>
+         <div class="materialContainer">
+            <div class="boxMargin">
 
 
-<div class="box">
+               <div class="box">
+                  <div class="title">LOGIN</div>
+                  <div class="input">
+                     <label for="name">Username</label>
+                     <input type="text" name="name" id="name" />
+                     <span class="spin"></span>
+                  </div>
 
-   <div class="title">LOGIN</div>
+                  <div class="input">
+                     <label for="pass">Password</label>
+                     <input type="password" name="pass" id="pass" />
+                     <span class="spin"></span>
+                  </div>
 
-   <div class="input">
-      <label for="name">Username</label>
-      <input type="text" name="name" id="name"/>
-      <span class="spin"></span>
-   </div>
+                  <div class="button login">
+                     <button><span>GO</span> <i class="fa fa-check"></i></button>
+                  </div>
 
-   <div class="input">
-      <label for="pass">Password</label>
-      <input type="password" name="pass" id="pass"/>
-      <span class="spin"></span>
-   </div>
+                  <a href="www.google.com" class="pass-forgot">Forgot your password?</a>
 
-   <div class="button login">
-      <button><span>GO</span> <i class="fa fa-check"></i></button>
-   </div>
+               </div>
+            </div>
 
-   <a href="www.google.com" class="pass-forgot">Forgot your password?</a>
+            <div class="overbox">
+               <div class="material-button alt-2"><span class="shape"></span></div>
 
-</div>
+               <div class="title">REGISTER</div>
 
-<div class="overbox">
-   <div class="material-button alt-2"><span class="shape"></span></div>
+               <div class="input">
+                  <label for="regname">Username</label>
+                  <input type="text" name="regname" id="regname" />
+                  <span class="spin"></span>
+               </div>
 
-   <div class="title">REGISTER</div>
+               <div class="input">
+                  <label for="regpass">Password</label>
+                  <input type="password" name="regpass" id="regpass" />
+                  <span class="spin"></span>
+               </div>
 
-   <div class="input">
-      <label for="regname">Username</label>
-      <input type="text" name="regname" id="regname"/>
-      <span class="spin"></span>
-   </div>
+               <div class="input">
+                  <label for="reregpass">Repeat Password</label>
+                  <input type="password" name="reregpass" id="reregpass" />
+                  <span class="spin"></span>
+               </div>
 
-   <div class="input">
-      <label for="regpass">Password</label>
-      <input type="password" name="regpass" id="regpass"/>
-      <span class="spin"></span>
-   </div>
-
-   <div class="input">
-      <label for="reregpass">Repeat Password</label>
-      <input type="password" name="reregpass" id="reregpass"/>
-      <span class="spin"></span>
-   </div>
-
-   <div class="button">
-      <button><span>NEXT</span></button>
-   </div>
+               <div class="button">
+                  <button><span>NEXT</span></button>
+               </div>
 
 
-</div>
+            </div>
 
-</div>
-    </div>
-  );
+         </div>
+      </div>
+   );
 }
 
 export default MaterialContainer;
