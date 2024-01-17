524357e22db5ef99be23f8748d743a06b27f514b
iamnihal21 <104316581+iamnihal21@users.noreply.github.com>
2024-01-17 12:25:06 +0530

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,77 +1,81 @@
 import React from "react";
-import "../style/login.css";
-import "../JS/loginApp";
+import Registration from "./registration";
 
-function MaterialContainer() {
+export default function login() {
   return (
-    <div>
-      <div class="HeaderSec">
-        <h1>Welcome to Attendance System</h1>
-      </div>
-      <div class="materialContainer">
-        <div class="boxMargin">
-          <div class="box">
-            <div class="title">LOGIN</div>
-            <div class="input">
-              <label for="name">Username</label>
-              <input type="text" name="name" id="name" />
-              <span class="spin"></span>
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStIQqC2-3n1m-S2ndc0x9XJmoMQk7G4XPqWA&usqp=CAU"
+            alt="Your Company"
+            style={{borderRadius : '8px'}}
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
 
-            <div class="input">
-              <label for="pass">Password</label>
-              <input type="password" name="pass" id="pass" />
-              <span class="spin"></span>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
 
-            <div class="button login">
-              <button>
-                <span>GO</span> <i class="fa fa-check"></i>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
               </button>
             </div>
+          </form>
 
-            <a href="www.google.com" class="pass-forgot">
-              Forgot your password?
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <a href="/Registration" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Create Account
             </a>
-          </div>
-        </div>
-
-        <div class="overbox">
-          <div class="material-button alt-2">
-            <span class="shape"></span>
-          </div>
-
-          <div class="title reg">
-            <p>REGISTER</p>
-          </div>
-
-          <div class="input">
-            <label for="regname">Username</label>
-            <input type="text" name="regname" id="regname" />
-            <span class="spin"></span>
-          </div>
-
-          <div class="input">
-            <label for="regpass">Password</label>
-            <input type="password" name="regpass" id="regpass" />
-            <span class="spin"></span>
-          </div>
-
-          <div class="input">
-            <label for="reregpass">Repeat Password</label>
-            <input type="password" name="reregpass" id="reregpass" />
-            <span class="spin"></span>
-          </div>
-         
-          <div class="button">
-            <button>
-              <span>NEXT</span>
-            </button>
-          </div>
+          </p>
         </div>
       </div>
-    </div>
-  );
+    </>
+  )
 }
-
-export default MaterialContainer;
