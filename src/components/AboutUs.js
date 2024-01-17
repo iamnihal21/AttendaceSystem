6d9f8fd614ee30ec4ae1448207237f4c24c6e5c0
iamnihal21 <104316581+iamnihal21@users.noreply.github.com>
2024-01-17 12:24:40 +0530

    Add files via upload

---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/login.css";
+
+function Aboutus() {
+  return (
+    <div className="contactUs">
+      <h2>This is about Us</h2>
+    </div>
+  );
+}
+
+export default Aboutus;
