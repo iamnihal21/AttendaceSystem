9617fdeeb62bff38bb7f46593838524c0c8627db
iamnihal21 <104316581+iamnihal21@users.noreply.github.com>
2024-01-17 12:23:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
-import Login from './components/login';
+import React from "react";
+import Header from "./components/header";
+import GenerateReport from "./components/generateReport";
+import Login from "./components/login";
+import Footer from "./components/footer";
+import Registration from "./components/registration";
+import Aboutus from "./components/AboutUs";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ContactUs from "./components/contactUs";
 
 function App() {
+
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <>
+      <Router>
+        <main>
+          <Header />
+          <Routes>
+            <Route path="/" element={<GenerateReport />} />
+            <Route path="/generateReport" element={<GenerateReport />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/Aboutus" element={<Aboutus />} />
+            <Route path="/ContactUs" element={<ContactUs />} />
+          </Routes>
+          <Footer />
+        </main>
+      </Router>
+    </>
   );
 }
+
 export default App;
